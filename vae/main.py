965e182cf988d64d982fa7aae3c3770c67f70c88
Alican Bozkurt <alican@ece.neu.edu>
2017-12-20 18:31:05 -0500

    use distributions library

--- vae/main.py
+++ vae/main.py
@@ -5,9 +5,9 @@ import torch.utils.data
 from torch import nn, optim
 from torch.autograd import Variable
 from torch.nn import functional as F
+from torch.distributions import Normal, kl_divergence
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
-
 
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
@@ -56,22 +56,17 @@ class VAE(nn.Module):
         h1 = self.relu(self.fc1(x))
         return self.fc21(h1), self.fc22(h1)
 
-    def reparameterize(self, mu, logvar):
-        if self.training:
-          std = logvar.mul(0.5).exp_()
-          eps = Variable(std.data.new(std.size()).normal_())
-          return eps.mul(std).add_(mu)
-        else:
-          return mu
-
     def decode(self, z):
         h3 = self.relu(self.fc3(z))
         return self.sigmoid(self.fc4(h3))
 
     def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 784))
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
+        mu, logstd = self.encode(x.view(-1, 784))
+        if self.training:
+            z = Normal(mu, torch.exp(logstd)).rsample()
+        else:
+            z = mu
+        return self.decode(z), mu, logstd
 
 
 model = VAE()
@@ -79,14 +74,14 @@ if args.cuda:
     model.cuda()
 
 
-def loss_function(recon_x, x, mu, logvar):
+def loss_function(recon_x, x, mu, logstd):
     BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784))
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    KLD = kl_divergence(Normal(mu, logstd.exp()), Normal(torch.zeros_like(mu), torch.ones_like(logstd))).sum()
     # Normalise by same number of elements as in reconstruction
     KLD /= args.batch_size * 784
 
@@ -120,23 +115,24 @@ def train(epoch):
 
 
 def test(epoch):
-    model.eval()
-    test_loss = 0
-    for i, (data, _) in enumerate(test_loader):
-        if args.cuda:
-            data = data.cuda()
-        data = Variable(data, volatile=True)
-        recon_batch, mu, logvar = model(data)
-        test_loss += loss_function(recon_batch, data, mu, logvar).data[0]
-        if i == 0:
-          n = min(data.size(0), 8)
-          comparison = torch.cat([data[:n],
-                                  recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
-          save_image(comparison.data.cpu(),
-                     'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+    with torch.no_grad():
+        model.eval()
+        test_loss = 0
+        for i, (data, _) in enumerate(test_loader):
+            if args.cuda:
+                data = data.cuda()
+            data = Variable(data)
+            recon_batch, mu, logvar = model(data)
+            test_loss += loss_function(recon_batch, data, mu, logvar).data[0]
+            if i == 0:
+                n = min(data.size(0), 8)
+                comparison = torch.cat([data[:n],
+                                        recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
+                save_image(comparison.data.cpu(),
+                           'results/reconstruction_' + str(epoch) + '.png', nrow=n)
 
-    test_loss /= len(test_loader.dataset)
-    print('====> Test set loss: {:.4f}'.format(test_loss))
+        test_loss /= len(test_loader.dataset)
+        print('====> Test set loss: {:.4f}'.format(test_loss))
 
 
 for epoch in range(1, args.epochs + 1):
@@ -144,7 +140,7 @@ for epoch in range(1, args.epochs + 1):
     test(epoch)
     sample = Variable(torch.randn(64, 20))
     if args.cuda:
-       sample = sample.cuda()
+        sample = sample.cuda()
     sample = model.decode(sample).cpu()
     save_image(sample.data.view(64, 1, 28, 28),
                'results/sample_' + str(epoch) + '.png')
